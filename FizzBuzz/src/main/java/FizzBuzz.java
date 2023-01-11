98aac69553f45100029479c324a263566837f3f4
jhigherevature <joseph.highe@revature.com>
2023-01-11 16:23:09 +0000

    Saved progress on FizzBuzz 2023-01-11 16:23:09 +0000

--- FizzBuzz/src/main/java/FizzBuzz.java
+++ FizzBuzz/src/main/java/FizzBuzz.java
@@ -15,6 +15,13 @@ public class FizzBuzz {
      * divisible by both, return 'FizzBuzz'. If none of these are true, return a blank string "".
      */
     public String FizzBuzzGen(int n){
-        return "";
+        String output = "";
+        if (n % 3 == 0)
+            output += "Fizz";
+
+        if (n % 5 == 0)
+            output += "Buzz";
+
+        return output;
     }
 }
