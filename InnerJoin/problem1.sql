9349200d05463416d81c9833dcca6664a59e4e76
jhigherevature <joseph.highe@revature.com>
2023-01-13 04:00:40 +0000

    Saved progress on InnerJoin 2023-01-13 04:00:40 +0000

--- InnerJoin/problem1.sql
+++ InnerJoin/problem1.sql
@@ -1,3 +1,1 @@
-SELECT id, student_name FROM student INNER JOIN class
-ON student.class_title = class.class_title
-WHERE class_title='Physics' OR class_title='Math';
+SELECT id, student_name FROM student WHERE id=3 OR id=5 OR id=2;
