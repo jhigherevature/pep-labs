af1d8a7eeaa064b6983288dfab0d0157bef24c64
jhigherevature <joseph.highe@revature.com>
2023-01-13 04:23:14 +0000

    Saved progress on InnerJoin 2023-01-13 04:23:14 +0000

--- InnerJoin/problem1.sql
+++ InnerJoin/problem1.sql
@@ -1,7 +1,1 @@
-/* This test can be cheesed... */
-SELECT id, student_name FROM student WHERE id=3 OR id=5 OR id=2;
-
-/* The following is correct, but the test seems broken? */
-SELECT student.id as id, student.student_name as student_name FROM student INNER JOIN class
-ON student.class_title = class.class_title
-WHERE student.class_title = 'Physics' OR student.class_title = 'Math';
+SELECT student.id as id, student.student_name as student_name FROM student INNER JOIN class ON student.class_title = class.class_title WHERE student.class_title = 'Physics' OR student.class_title = 'Math';
