5e58efa38b6be02d52203c7cff93b80e6bbb8f80
jhigherevature <joseph.highe@revature.com>
2023-01-14 23:38:15 +0000

    Saved progress on Fibonacci 2023-01-14 23:38:15 +0000

--- Fibonacci/src/main/java/Fibonacci.java
+++ Fibonacci/src/main/java/Fibonacci.java
@@ -15,6 +15,17 @@ public class Fibonacci {
      * @return the nth number of fibonacci sequence.
      */
     public int fib(int n){
-        return 0;
+        int[] arr = new int[n];
+        return memoFib(arr, n);
+    }
+
+    public int memoFib(int[] arr, int n) {
+        if (n <= 0) return 0;
+        if (n == 1) return 1;
+
+        if (arr[n-1] != 0)
+            return arr[n-1];
+
+        return arr[n-1] = memoFib(arr, n-1) + memoFib(arr, n-2);
     }
 }
